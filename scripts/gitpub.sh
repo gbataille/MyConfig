676a097fb67d1f0f50f469564e90532132fb65bb
Gregory Bataille <gbataille.dev@gmail.com>
2012-12-27 15:54:14 +0100

    Adding a default branch name

--- scripts/gitpub.sh
+++ scripts/gitpub.sh
@@ -1,5 +1,12 @@
 #!/bin/ksh
-git co master
-git merge master $1
-git push
-git co $1
+# git co master
+# git merge master $1
+# git push
+# git co $1
+branch=$1
+
+if [ -z "$branch" ]
+then
+  branch="wip"
+fi
+echo $branch
