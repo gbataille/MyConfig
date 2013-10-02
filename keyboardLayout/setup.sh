d303755f674c9063ef783b8098bc642bce20ad2d
Gregory Bataille <gbataille.dev@gmail.com>
2013-10-02 14:32:10 +0200

    packaging

---
+++ keyboardLayout/setup.sh
@@ -0,0 +1,3 @@
+sudo cp ./dvorakUsAccent.keylayout "/Library/Keyboard Layouts/"
+sudo rm /var/db/.AppleSetupDone
+sudo "/System/Library/CoreServices/Setup Assistant.app/Contents/MacOS/Setup Assistant"
