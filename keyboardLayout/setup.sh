252d1bf8020bc6bb67593e76ee5ffbe6e1091024
Gregory Bataille <gbataille.dev@gmail.com>
2014-03-26 16:02:46 +0100

    Not touching the low level system setup

--- keyboardLayout/setup.sh
+++ keyboardLayout/setup.sh
@@ -1,3 +1,1 @@
 sudo cp ./dvorakUsAccent.keylayout "/Library/Keyboard Layouts/"
-sudo rm /var/db/.AppleSetupDone
-sudo "/System/Library/CoreServices/Setup Assistant.app/Contents/MacOS/Setup Assistant"
