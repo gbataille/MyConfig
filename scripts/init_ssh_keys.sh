e6b4c2eb3a9574feae5b7b104c23b68e967cf759
Grégory Bataille <gregory.bataille@gmail.com>
2020-02-06 09:53:38 +0100

    load whichever ssh_key is defined

--- scripts/init_ssh_keys.sh
+++ scripts/init_ssh_keys.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-existing_key=`ssh-add -l | grep "gregory.bataille@gmail.com"`
+existing_key=`ssh-add -l | grep "gregory.bataille"`
 if [ -z "$existing_key" ]; then
   echo -e "SSH key not loaded - doing it now\n"
   ssh-add ~/.ssh/id_rsa
