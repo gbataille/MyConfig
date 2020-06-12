156619d5d3d4ca2e3c1fab75accd58f80dc2139d
Grégory Bataille <gregory.bataille@gmail.com>
2020-06-12 06:49:00 +0200

    auto setup /etc/hosts on terminal start

--- scripts/setup_host_file.sh
+++ scripts/setup_host_file.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+echo "Setting up local.retinai.com with your local IP"
+
+dns=local.retinai.com
 ip=`ifconfig en0 | grep inet | grep -v inet6 | cut -d ' ' -f2`
-sudo sed -i '' "s/.*local.retinai.com/${ip} local.retinai.com/" /etc/hosts
+existing_ip=`grep $dns /etc/hosts | awk '{print $1}'`
+
+echo "IP: ${ip}"
+if [ ! "${existing_ip}" == "${ip}" ]; then
+  echo "Need to update /etc/hosts. You'll be asked for the computer's sudo password"
+  sudo sed -i '' "s/.*${dns}/${ip} ${dns}/" /etc/hosts
+else
+  echo "Setup already correct"
+fi
