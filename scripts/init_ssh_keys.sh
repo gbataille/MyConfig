004d8402e54cbcabaa4218e87831545f4a37239a
Grégory Bataille <gregory.bataille@gmail.com>
2020-12-26 15:08:49 +0100

    load secondary ssh key on startup

--- scripts/init_ssh_keys.sh
+++ scripts/init_ssh_keys.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 existing_key=`ssh-add -l | grep "gregory.bataille"`
 if [ -z "$existing_key" ]; then
-  echo -e "SSH key not loaded - doing it now\n"
+  echo -e "Main SSH key not loaded - doing it now\n"
   ssh-add ~/.ssh/id_rsa
+fi
+gits_key=`ssh-add -l | grep "guyintheshell"`
+if [ -z "$gits_key" ]; then
+  echo -e "GITS SSH key not loaded - doing it now\n"
+  ssh-add ~/.ssh/id_gits
 fi
