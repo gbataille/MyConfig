c4412308daba77ffde98a4234b09994e9354c84e
Grégory Bataille <gregory.bataille@gmail.com>
2020-12-27 09:05:44 +0100

    clear after gits mode

--- scripts/set_gits.sh
+++ scripts/set_gits.sh
@@ -2,3 +2,4 @@
 export SHELL_USER=GuyInTheShell
 tmux set pane-border-status off
 tmux set -g status-left '#[fg=colour235,bg=colour252,bold] ❐ #S #[fg=colour252,bg=colour238,nobold] #[fg=colour245,bg=colour238,bold] GuyInTheShell #[fg=colour238,bg=black,nobold] '
+clear
