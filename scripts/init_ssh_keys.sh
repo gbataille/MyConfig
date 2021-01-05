4a60001a6e83f78092bac4b1ea6e33e037dabe92
Grégory Bataille <gregory.bataille@gmail.com>
2021-01-05 19:44:25 +0100

    SDC github key

--- scripts/init_ssh_keys.sh
+++ scripts/init_ssh_keys.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-existing_key=`ssh-add -l | grep "gregory.bataille"`
+existing_key=`ssh-add -l | grep "gregory.bataille@gmail.com"`
 if [ -z "$existing_key" ]; then
   echo -e "Main SSH key not loaded - doing it now\n"
   ssh-add ~/.ssh/id_ed25519
@@ -8,4 +8,9 @@ gits_key=`ssh-add -l | grep "guyintheshell"`
 if [ -z "$gits_key" ]; then
   echo -e "GITS SSH key not loaded - doing it now\n"
   ssh-add ~/.ssh/id_gits
+fi
+gits_key=`ssh-add -l | grep "gregory.bataille@swissdecode.com"`
+if [ -z "$gits_key" ]; then
+  echo -e "GITS SSH key not loaded - doing it now\n"
+  ssh-add ~/.ssh/id_sdc
 fi
