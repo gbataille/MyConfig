14213f5ff5f4a64a74f8a8ea5be754f792f8eda7
Grégory Bataille <gregory.bataille@gmail.com>
2021-01-08 06:12:04 +0100

    auto load all ssh keys from keychain

--- scripts/init_ssh_keys.sh
+++ scripts/init_ssh_keys.sh
@@ -1,16 +1,2 @@
 #!/bin/zsh
-existing_key=`ssh-add -l | grep "gregory.bataille@gmail.com"`
-if [ -z "$existing_key" ]; then
-  echo -e "Main SSH key not loaded - doing it now\n"
-  ssh-add ~/.ssh/id_ed25519
-fi
-gits_key=`ssh-add -l | grep "guyintheshell"`
-if [ -z "$gits_key" ]; then
-  echo -e "GITS SSH key not loaded - doing it now\n"
-  ssh-add ~/.ssh/id_gits
-fi
-gits_key=`ssh-add -l | grep "gregory.bataille@swissdecode.com"`
-if [ -z "$gits_key" ]; then
-  echo -e "GITS SSH key not loaded - doing it now\n"
-  ssh-add ~/.ssh/id_sdc
-fi
+ssh-add -A
